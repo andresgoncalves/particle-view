6c37f8889f6244c8ae64c63628b0e7e6be571fd4
Andres Goncalves <88066799+andresgoncalves@users.noreply.github.com>
2024-12-17 19:06:04 -0400

    chore: :heavy_plus_sign: setup qt libraries

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,13 @@
-#include <iostream>
+#include <QApplication>
+#include <QLabel>
 
-int main(int, char**){
-    std::cout << "Hello, from particle-view!\n";
+int main(int argc, char **argv)
+{
+    auto app = QApplication{argc, argv};
+
+    auto label = QLabel("Hello, from particle-view!");
+    label.setContentsMargins(24, 16, 24, 16);
+    label.show();
+
+    return app.exec();
 }
