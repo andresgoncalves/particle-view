66d658113210ba9bdd4c7600b8f6c160709d35a0
Andres Goncalves <88066799+andresgoncalves@users.noreply.github.com>
2024-12-19 21:33:22 -0400

    feat: :sparkles: added render controller transformations

--- src/controllers/RenderController.cpp
+++ src/controllers/RenderController.cpp
@@ -2,48 +2,162 @@
 
 RenderController::RenderController() {}
 
-void RenderController::rotateX(float angle)
+void RenderController::rotate(QVector3D angles, ReferenceFrame referenceFrame)
 {
-  auto vector = viewProjectionMatrix.inverted() * QVector3D{1.0f, 0.0f, 0.0f};
-  viewProjectionMatrix.rotate(angle, vector);
+  dirtyViewProjectionMatrix = true;
+  if (referenceFrame == Projection)
+  {
+    auto currentRotationMatrix = QQuaternion::fromEulerAngles(rotationAngles).toRotationMatrix();
+    auto newRotationMatrix = QQuaternion::fromEulerAngles(angles).toRotationMatrix();
+
+    rotationAngles = QQuaternion::fromRotationMatrix(newRotationMatrix * currentRotationMatrix).toEulerAngles();
+  }
+  else
+  {
+    rotationAngles += angles;
+  }
 }
 
-void RenderController::rotateY(float angle)
+void RenderController::translate(QVector3D vector, ReferenceFrame referenceFrame)
 {
-  auto vector = viewProjectionMatrix.inverted() * QVector3D{0.0f, 1.0f, 0.0f};
-  viewProjectionMatrix.rotate(angle, vector);
+  dirtyViewProjectionMatrix = true;
+  if (referenceFrame == Projection)
+  {
+    auto currentRotationMatrix = QMatrix4x4{QQuaternion::fromEulerAngles(rotationAngles).toRotationMatrix()};
+
+    translationVector += scaleFactor * currentRotationMatrix.inverted().mapVector(vector);
+  }
+  else
+  {
+    translationVector += vector;
+  }
 }
 
-void RenderController::rotateZ(float angle)
+void RenderController::scale(float factor, ReferenceFrame referenceFrame)
 {
-  auto vector = viewProjectionMatrix.inverted() * QVector3D{0.0f, 0.0f, 1.0f};
-  viewProjectionMatrix.rotate(angle, vector);
+  dirtyViewProjectionMatrix = true;
+  if (referenceFrame == Projection)
+  {
+    scaleFactor *= factor;
+  }
+  else
+  {
+    scaleFactor += factor;
+  }
 }
 
-void RenderController::translateX(float distance)
+void RenderController::setRotation(QVector3D angles)
 {
-  auto vector = viewProjectionMatrix.inverted() * QVector3D{1.0f, 0.0f, 0.0f};
-  viewProjectionMatrix.translate(distance * vector);
+  dirtyViewProjectionMatrix = true;
+  rotationAngles = angles;
+};
+
+void RenderController::setTranslation(QVector3D vector)
+{
+  dirtyViewProjectionMatrix = true;
+  translationVector = vector;
+};
+
+void RenderController::setScale(float factor)
+{
+  dirtyViewProjectionMatrix = true;
+  scaleFactor = factor;
+};
+
+void RenderController::rotateX(float angle, ReferenceFrame referenceFrame)
+{
+  rotate({angle, 0.0f, 0.0f}, referenceFrame);
 }
 
-void RenderController::translateY(float distance)
+void RenderController::rotateY(float angle, ReferenceFrame referenceFrame)
 {
-  auto vector = viewProjectionMatrix.inverted() * QVector3D{0.0f, 1.0f, 0.0f};
-  viewProjectionMatrix.translate(distance * vector);
+  rotate({0.0f, angle, 0.0f}, referenceFrame);
 }
 
-void RenderController::translateZ(float distance)
+void RenderController::rotateZ(float angle, ReferenceFrame referenceFrame)
 {
-  auto vector = viewProjectionMatrix.inverted() * QVector3D{0.0f, 0.0f, 1.0f};
-  viewProjectionMatrix.translate(distance * vector);
+  rotate({0.0f, 0.0f, angle}, referenceFrame);
 }
 
-void RenderController::scale(float scale)
+void RenderController::translateX(float distance, ReferenceFrame referenceFrame)
 {
-  viewProjectionMatrix *= scale;
+  translate({distance, 0.0f, 0.0f}, referenceFrame);
 }
+
+void RenderController::translateY(float distance, ReferenceFrame referenceFrame)
+{
+  translate({0.0f, distance, 0.0f}, referenceFrame);
+}
+
+void RenderController::translateZ(float distance, ReferenceFrame referenceFrame)
+{
+  translate({0.0f, 0.0f, distance}, referenceFrame);
+}
+
+void RenderController::setRotationX(float angle)
+{
+  dirtyViewProjectionMatrix = true;
+  rotationAngles.setX(angle);
+}
+
+void RenderController::setRotationY(float angle)
+{
+  dirtyViewProjectionMatrix = true;
+  rotationAngles.setY(angle);
+}
+
+void RenderController::setRotationZ(float angle)
+{
+  dirtyViewProjectionMatrix = true;
+  rotationAngles.setZ(angle);
+}
+
+void RenderController::setTranslationX(float distance)
+{
+  dirtyViewProjectionMatrix = true;
+  translationVector.setX(distance);
+}
+
+void RenderController::setTranslationY(float distance)
+{
+  dirtyViewProjectionMatrix = true;
+  translationVector.setY(distance);
+}
+
+void RenderController::setTranslationZ(float distance)
+{
+  dirtyViewProjectionMatrix = true;
+  translationVector.setZ(distance);
+}
+
+QVector3D RenderController::getRotation()
+{
+  return rotationAngles;
+};
+
+QVector3D RenderController::getTranslation()
+{
+  return translationVector;
+};
+
+float RenderController::getScale()
+{
+  return scaleFactor;
+};
 
 QMatrix4x4 RenderController::getViewProjectionMatrix()
 {
+  if (dirtyViewProjectionMatrix)
+  {
+    dirtyViewProjectionMatrix = false;
+
+    /* Rotate */
+    viewProjectionMatrix = QMatrix4x4{QQuaternion::fromEulerAngles(rotationAngles).toRotationMatrix()};
+    /* Translate */
+    viewProjectionMatrix.translate(translationVector);
+    /* Scale */
+    viewProjectionMatrix.scale(scaleFactor);
+  }
+
   return viewProjectionMatrix;
 }
--- src/controllers/RenderController.h
+++ src/controllers/RenderController.h
@@ -6,22 +6,51 @@
 class RenderController
 {
 public:
+  enum ReferenceFrame
+  {
+    Model,
+    Projection
+  };
+
   RenderController();
 
-  void rotateX(float angle);
-  void rotateY(float angle);
-  void rotateZ(float angle);
+  void rotate(QVector3D angles, ReferenceFrame referenceFrame = Model);
+  void translate(QVector3D vector, ReferenceFrame referenceFrame = Model);
+  void scale(float factor, ReferenceFrame referenceFrame = Model);
 
-  void translateX(float distance);
-  void translateY(float distance);
-  void translateZ(float distance);
+  void setRotation(QVector3D angles);
+  void setTranslation(QVector3D vector);
+  void setScale(float factor);
 
-  void scale(float scale);
+  void rotateX(float angle, ReferenceFrame referenceFrame = Model);
+  void rotateY(float angle, ReferenceFrame referenceFrame = Model);
+  void rotateZ(float angle, ReferenceFrame referenceFrame = Model);
+
+  void translateX(float distance, ReferenceFrame referenceFrame = Model);
+  void translateY(float distance, ReferenceFrame referenceFrame = Model);
+  void translateZ(float distance, ReferenceFrame referenceFrame = Model);
+
+  void setRotationX(float angle);
+  void setRotationY(float angle);
+  void setRotationZ(float angle);
+
+  void setTranslationX(float distance);
+  void setTranslationY(float distance);
+  void setTranslationZ(float distance);
+
+  QVector3D getRotation();
+  QVector3D getTranslation();
+  float getScale();
 
   QMatrix4x4 getViewProjectionMatrix();
 
 private:
+  bool dirtyViewProjectionMatrix = true;
   QMatrix4x4 viewProjectionMatrix;
+
+  QVector3D rotationAngles;
+  QVector3D translationVector;
+  float scaleFactor = 1.0f;
 };
 
 #endif
